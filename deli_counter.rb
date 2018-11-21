da52da18c9e2e83a9acbd54cd4a4aa90a2ee47fd
karen ercoli <karenercoli@gmail.com>
2018-11-21 13:02:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
+def line(katz_deli)
+  new_katz_deli= []
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |name, index|
+      new_katz_deli<<"#{index + 1}. #{name}"
+end
+puts "The line is currently: #{new_katz_deli.join(" ")}"
+end
+end
+
+def take_a_number(katz_deli,name)
+if katz_deli.count == 0
+    katz_deli<<name
+  puts "Welcome, #{name}. You are number 1 in line."
+else
+  katz_deli.each do |name|
+  puts "Welcome #{name}. You are number #{katz_deli.size} in line."
+end
+end
+end
 # Write your code here.
+def now_serving(katz_deli)
+if katz_deli.count == 0
+puts "The line is empty"
+else
+end
+end
